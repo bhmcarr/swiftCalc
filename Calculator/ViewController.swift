1033a405fa3c19aa7d151d0e191b76b4d814ab5a
bhmcarr <bhmcarr@gmail.com>
2018-09-15 11:32:21 -0400

    Completely re-do the implementation, it works

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,100 +11,125 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var screenLabel: UILabel!
+    @IBOutlet weak var divisionButton: UIButton!
+    @IBOutlet weak var multiplyButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
     
     // globals
-    var numberBuffer: String = ""
-    var numberArray: [Int] = []
-    var opArray: [Character] = []
+    var displayBuffer: String = ""
+    var op: Character = "i"
+    
+    var numArray: [Int] = []
+    var runningValue: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     @IBAction func numberPressed(_ sender: UIButton) {
-        numberBuffer.append("\(sender.tag)")
+        displayBuffer.append("\(sender.tag)")
         updateScreen()
+
+        resetOpButtonColor()
     }
     
     @IBAction func opPressed(_ sender: UIButton) {
-        if numberBuffer == "" {
-            return
+        // op pressed, store current value if it exists
+        if !displayBuffer.isEmpty{
+            numArray.append(Int(displayBuffer)!)
         }
-        numberArray.append(Int(numberBuffer)!)
-        // put operator into the opArray
-        switch sender.tag {
-        case 10: // +
-            opArray.append("+")
-            print ("Added + to opArray")
-        case 11: // -
-            opArray.append("-")
-            print ("Added - to opArray")
-        case 12: // *
-            opArray.append("*")
-            print ("Added * to opArray")
-        case 13: // /
-            opArray.append("/")
-            print ("Added / to opArray")
-        default:
-            print("Something went wrong")
-        }
-        clearBuffer()
-    }
-    
-    @IBAction func equalsPressed(_ sender: UIButton) {
-        // grab the last value from numberBuffer and put it into numberArray
-        if numberBuffer != "" {
-            numberArray.append(Int(numberBuffer)!)
-        }
-        // iterate through all values in opArray and do each operation
-        var runningValue: Int = numberArray[0]
-        numberArray.removeFirst()
         
-        for op in opArray {
+        if op != "i" {
+            // do math and update screen and running value
             switch op {
             case "+":
-                runningValue += numberArray[0]
+                numArray[0] = numArray[0] + numArray[1]
             case "-":
-                runningValue -= numberArray[0]
+                numArray[0] = numArray[0] - numArray[1]
             case "*":
-                runningValue *= numberArray[0]
+                numArray[0] = numArray[0] * numArray[1]
             case "/":
-                runningValue /= numberArray[0]
+                numArray[0] = numArray[0] / numArray[1]
             default:
-                print("Something happened.")
+                print("Something went wrong")
             }
-            numberArray.removeFirst()
+            
+            // update screen
+            displayBuffer = ("\(numArray[0])")
+            updateScreen()
+            
+            // remove numArray[1]
+            numArray.remove(at: 1)
+            
+            // unset op
+            op = "i"
         }
-        opArray.removeAll()
-        numberBuffer = "\(runningValue)"
-        updateScreen()
+        
+        // set op mode
+        if sender.tag != 14 {
+            sender.backgroundColor = hexStringToUIColor(hex: "#e74c3c")
+        }
+        switch sender.tag {
+        case 10:
+            op = "+"
+        case 11:
+            op = "-"
+        case 12:
+            op = "*"
+        case 13:
+            op = "/"
+        case 14: // =
+            // equals pressed, put runningValue into buffer + update screen
+            op = "i"
+            displayBuffer = "\(numArray[0])"
+            updateScreen()
+        default:
+            print("operator setting problem")
+        }
+        
+        displayBuffer = ""
     }
     
     @IBAction func clearPressed(_ sender: UIButton) {
-        clearBuffer()
-        clearOp()
+        displayBuffer = ""
+        numArray.removeAll()
         updateScreen()
     }
     
     func updateScreen() {
-        screenLabel.text = numberBuffer
-        print("Screen updated as \(numberBuffer)")
+        screenLabel.text = displayBuffer
     }
     
-    func clearBuffer() {
-        numberBuffer = ""
-        print("Number buffer cleared.")
+    func resetOpButtonColor() {
+        plusButton.backgroundColor = hexStringToUIColor(hex: "#F1C40F")
+        minusButton.backgroundColor = hexStringToUIColor(hex: "#F1C40F")
+        multiplyButton.backgroundColor = hexStringToUIColor(hex: "#F1C40F")
+        divisionButton.backgroundColor = hexStringToUIColor(hex: "#F1C40F")
     }
     
-    func clearOp() {
-        opArray.removeAll()
-        print("Op array cleared.")
-    }
-    
-    func clearNumber() {
-        numberArray.removeAll()
-        print("Number array cleared")
+    func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
     }
 
 }
